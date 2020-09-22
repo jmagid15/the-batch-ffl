2389dd216b25e6ee0166aec39bfba514e6a1c449
Jake Magid <jake.magid@gmail.com>
2020-09-22 13:41:36 -0400

    Remove logging

--- routes/teamsRoutes.js
+++ routes/teamsRoutes.js
@@ -3,7 +3,6 @@ const utils = require('./utils');
 
 module.exports = (app) => {
   app.get('/api/sheets/teams/:teamID', async (req, res) => {
-    console.log('hitting teams endpoint');
     try {
       const rsp = await axios.get(
         'http://fantasy.espn.com/apis/v3/games/ffl/seasons/2020/segments/0/leagues/319300?view=mTeam'
