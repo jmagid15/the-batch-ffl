142bb3c91e3b8f39f1980e4427cc6dbb63a4f142
Jake Magid <jake.magid@gmail.com>
2020-10-04 14:08:31 -0400

    Update reports

--- client/src/components/Reports.js
+++ client/src/components/Reports.js
@@ -2,28 +2,70 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
 import author from '../images/author.png';
-import PorchV1 from '../reports/the-porch-volume-1.md';
-import { H5 } from '@blueprintjs/core';
+import { PorchV1, PorchV2 } from '../reports';
+import { H5, Tabs, Tab } from '@blueprintjs/core';
 
 class Reports extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { markdown: '' };
-  }
+  state = {
+    markdown: '',
+    mostRecentReport: 2,
+    volumes: {
+      1: PorchV1,
+      2: PorchV2,
+    },
+    windowDimensions: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    },
+  };
 
   componentDidMount() {
     // Get the contents from the Markdown file and put them in the React state, so we can reference it in render() below.
-    fetch(PorchV1)
+    fetch(this.state.volumes[this.state.mostRecentReport])
       .then((res) => res.text())
       .then((text) => this.setState({ markdown: text }));
+    // Detect when window has been resized
+    window.addEventListener('resize', this.updateWindowDimensions);
   }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  handleTabChange = (newTabId, prevTabId) => {
+    fetch(this.state.volumes[newTabId])
+      .then((res) => res.text())
+      .then((text) => this.setState({ markdown: text }));
+  };
+
+  updateWindowDimensions = () => {
+    this.setState({
+      ...this.state,
+      windowDimensions: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+      },
+    });
+    console.log(this.state.windowDimensions);
+  };
 
   render() {
     return (
       <div className="report-view">
-        <div className="report-author">
+        <div className="report-nav">
+          <div className="tabs-container">
+            <Tabs
+              id="vol-tabs"
+              vertical={this.state.windowDimensions.width > 620}
+              defaultSelectedTabId={this.state.mostRecentReport}
+              onChange={this.handleTabChange}
+            >
+              <Tab id="1" title="Volume 1" className="tab-item" />
+              <Tab id="2" title="Volume 2" className="tab-item" />
+            </Tabs>
+          </div>
           <h2>About the author</h2>
-          <img src={author} height="400" />
+          <img src={author} alt="Hoom" />
           <H5>I'm Horny</H5>
         </div>
         <div className="report-doc">
---
+++ client/src/reports/index.js
@@ -0,0 +1,2 @@
+export { default as PorchV1 } from './the-porch-volume-1.md';
+export { default as PorchV2 } from './the-porch-volume-2.md';
